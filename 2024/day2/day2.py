dfa80d6acc8b57ac77e66b570162ebf4b647c355
Morgan <morno368@student.liu.se>
2024-12-02 07:42:26 +0100

    another one

--- 2024/day2/day2.py
+++ 2024/day2/day2.py
@@ -1,9 +1,27 @@
+from itertools import pairwise
+
+
 def part_one():
-    pass
+    def increasing(nums):
+        return all([-4 < a - b < 0 for a,b in pairwise(nums)])
+    def decreasing(nums):
+        return all([0 < a - b < 4 for a,b in pairwise(nums)])
+
+    f = open("input.txt", "r").read().splitlines()
+    f = [list(map(int , (l.rstrip().split(" ")))) for l in f]
+    return sum(increasing(l) or decreasing(l) for l in f)
 
 
 def part_two():
-    pass
+    def increasing(nums):
+        return all([-4 < a - b < 0 for a,b in pairwise(nums)])
+    def decreasing(nums):
+        return all([0 < a - b < 4 for a,b in pairwise(nums)])
+
+    f = open("input.txt", "r").read().splitlines()
+    f = [list(map(int , (l.rstrip().split(" ")))) for l in f]
+    
+    return sum(1 for l in f if any(increasing(l[:i] + l[i+1:]) or decreasing(l[:i] + l[i+1:]) for i in range(len(l))))
 
 
 if __name__ == "__main__":
