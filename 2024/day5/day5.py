330cb6c2c84a012c7c81eace2f135303ec9529d7
Morgan <morno368@student.liu.se>
2024-12-05 09:54:26 +0100

    progress on part 2, needs debugging... might not bother for a long time

--- 2024/day5/day5.py
+++ 2024/day5/day5.py
@@ -1,5 +1,5 @@
 def part_one():
-    f = open("input.txt", "r").read().split("\n\n")
+    f = open("test.txt", "r").read().split("\n\n")
     od = {}
     for p in f[0].splitlines():
         p = list(map(int, p.split('|')))
@@ -8,8 +8,8 @@ def part_one():
         else:
             od[p[1]].append(p[0])
     
-    print(od)
     sum = 0
+    rs = [] # collect input for part 2
     for l in f[1].splitlines():
         l = list(map(int, l.split(",")))
         seen = set()
@@ -24,38 +24,42 @@ def part_one():
             seen.add(e)
 
         if flag:
-            sum += l[len(l)//2] 
+            sum += l[len(l)//2]
+
+        if not flag: # collect input for part 2
+            rs.append(l)
     
-    return sum
+    return sum, rs, od
 
 
-def part_two():
-    f = open("input.txt", "r").read().split("\n\n")
-    od = {}
-    for p in f[0].splitlines():
-        p = list(map(int, p.split('|')))
-        if p[1] not in od:
-            od[p[1]] = [p[0]]
-        else:
-            od[p[1]].append(p[0])
-    
-    sum = 0
-    for l in f[1].splitlines():
-        l = list(map(int, l.split(",")))
-        seen = set()
-        for e in l:
-            if e in od:
-                r = set(l).intersection(od[e])
-                if not r.issubset(seen):
-                    #TODO
-                    #fix rows
-                    pass
+def part_two(rows, deps):
+    fixed = []  
+       
+    for row in rows:
+        # KAHN'S ALGORTHM
+        # https://en.wikipedia.org/wiki/Topological_sorting
+        deps_c = deps.copy()
+        s = set([node for node in row if node not in deps_c or all(dep not in row for dep in deps_c[node])])
+        l = []
+        while s:
+            print(f"set: {s}")
+            print(f"list: {l}")
+
+            n = s.pop()
+            l.append(n)
             
-            seen.add(e)
+            for m in list(deps_c.keys()):
+                if n in deps_c[m]:
+                    deps_c[m].remove(n)
+                if not deps_c[m]:
+                    s.add(m)
+
+        fixed.append(l)
     
-    return sum
+    return sum([r[len(r)//2] for r in fixed]) 
 
 
 if __name__ == "__main__":
-    print(f"Solution to part one {part_one()}")
-    print(f"Solution to part two {part_two()}")
+    s, rs, od = part_one()
+    print(f"Solution to part one {s}")
+    print(f"Solution to part two {part_two(rs, od)}")
