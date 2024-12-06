10ad0b39997384691bc73db60f6b6fa61a3f6ffb
Morgan <morno368@student.liu.se>
2024-12-06 11:17:30 +0100

    this problem was no fun

--- 2024/day6/day6.py
+++ 2024/day6/day6.py
@@ -1,31 +1,70 @@
-def part_one():
-    f = open("input.txt", "r").read()
-    f = f.splitlines()
-    w = len(f[0])
-    h = len(f)
-    visited = set()
-    dir = [0, -1]  
-    pos = find_start(f)
+from copy import deepcopy
 
-    visited.add((pos[0], pos[1]))  
+
+def part_one(m, pos, dir):
+    w = len(m[0])
+    h = len(m)
+    visited = set()
+    visited.add(tuple(pos))
 
     while True:
-        if pos[0] + dir[0] < 0 or pos[0] + dir[0] >= w or pos[1] + dir[1] < 0 or pos[1] + dir[1] >= h:
+        c_m = [pos[0] + dir[0], pos[1] + dir[1]]
+        if c_m[0] < 0 or c_m[0] >= w or c_m[1] < 0 or c_m[1] >= h:
             return len(visited)
+
+        if m[c_m[1]][c_m[0]] == '#':
+            dir = rotate(dir)  
+            continue  
+        
+        pos = c_m
+
+        visited.add(tuple(pos))  
+
+
+def part_two(m, pos, dir):
+    w = len(m[0])
+    h = len(m)
+    org_pos = deepcopy(pos)
+    org_dir = deepcopy(dir)
+
+    def walk(m, pos, dir):
+        path = set()
+        path.add((tuple(pos), tuple(dir)))
+        while True:
+            c_m = [pos[0] + dir[0], pos[1] + dir[1]]
+            
+            if c_m[0] < 0 or c_m[0] >= w or c_m[1] < 0 or c_m[1] >= h:
+                return 0
+
+            if m[c_m[1]][c_m[0]] == '#':
+                dir = rotate(dir)  
+                continue  
+            
+            pos = c_m
+            if (tuple(pos), tuple(dir)) in path:
+                return 1
+            
+            path.add((tuple(pos), tuple(dir)))  
+
+    loops = []
+    while True:
+        if pos[0] + dir[0] < 0 or pos[0] + dir[0] >= w or pos[1] + dir[1] < 0 or pos[1] + dir[1] >= h:
+            return len(set(loops))
 
         c_m = [pos[0] + dir[0], pos[1] + dir[1]]
 
-        if f[c_m[1]][c_m[0]] == '#':
+        if m[c_m[1]][c_m[0]] == '#':
             dir = rotate(dir)  
-            continue  
-
-        pos = c_m
-        visited.add((pos[0], pos[1]))  
-
-
-def part_two():
-    #TODO:
-    pass
+            continue
+        elif m[c_m[1]][c_m[0]] == '^':
+            pos = c_m
+            continue
+        else:
+            m_copy = deepcopy(m)
+            m_copy[c_m[1]][c_m[0]] = '#'
+            if walk(m_copy, org_pos, org_dir):
+                loops.append(tuple(c_m))
+            pos = c_m
 
 
 def find_start(m):
@@ -48,5 +87,10 @@ def rotate(dir):
 
 
 if __name__ == "__main__":
-    print(f"Solution to part one {part_one()}")
-    print(f"Solution to part two {part_two()}")
+    f = open("input.txt", "r").read()
+    m = f.splitlines()
+    m = list(map(list, m))
+    dir = [0, -1]  
+    pos = find_start(m)
+    print(f"Solution to part one {part_one(m, pos, dir)}")
+    print(f"Solution to part two {part_two(m, pos, dir)}")
